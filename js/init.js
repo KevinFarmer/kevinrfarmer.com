d3ae813e65cdcb01d48b203634e05269b14fb948
Kevin Farmer <kevin.farmer96@gmail.com>
2017-05-10 22:44:04 -0400

    Started to update site with Materialize

---
+++ js/init.js
@@ -0,0 +1,14 @@
+
+// code from:http://materializecss.com/modals.html#!
+// additional help from: http://stackoverflow.com/questions/41626343/materialized-modal-not-displaying
+// parallax code from: http://materializecss.com/parallax.html
+// this initializes all modals so they can be triggered
+$(document).ready(function() {
+  // code from:http://materializecss.com/modals.html#!
+  // additional help from: http://stackoverflow.com/questions/41626343/materialized-modal-not-displaying
+  // this initializes all modals so they can be triggered
+  $('.modal').modal();
+  $('.parallax').parallax();
+});
+
+$(".dropdown-button").dropdown({ hover: false });
